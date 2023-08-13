0576ee2c4da7cd880c6468d68de1ca1fc053bba0
dmrudy17 <dmr136@zips.uakron.edu>
2023-08-13 18:37:58 -0400

    remove .nuxt from repository

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/dmrudy17/VueSandbox/nuxt-tutorial/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default" | "products"
-declare module "/home/dmrudy17/VueSandbox/nuxt-tutorial/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
